7fb2c369e6db8a463cc970357a31553aafacefe2
Snehpatel007 <81500980+Snehpatel007@users.noreply.github.com>
2024-01-12 21:54:16 +0530

    Add files via upload

---
+++ client/src/component/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+       
+        <p>
+          Made with <i className="fas fa-heartbeat" /> by{" "}
+          
+            Sneh Patel | Mitanshu Patel |  Durgesh Suthar | Sahaj Tilala
+          .
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
